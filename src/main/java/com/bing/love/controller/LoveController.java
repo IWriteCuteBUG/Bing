13053ad471b1d9a6dbd5801ad2e22d7340abd124
zhaoyupeng@cie-china.com <zhaoyupeng@cie-china.com>
2020-02-15 00:17:40 +0800

    bing

--- src/main/java/com/bing/love/controller/LoveController.java
+++ src/main/java/com/bing/love/controller/LoveController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoveController {
 
-    @RequestMapping("love/bing")
+    @RequestMapping("/bing")
     public String love() {
         return "饼饼我爱你";
     }
